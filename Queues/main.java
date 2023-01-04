f62b5b40b66cefe8a4f00bc8993fc7de3ab96d4b
Praveen Pushkar <praveenpushkar140@gmail.com>
2023-01-05 03:06:41 +0530

    comiit

--- Queues/main.java
+++ Queues/main.java
@@ -4,6 +4,6 @@ public class main {
     public static void main(String[] args){
         System.out.println("hello worldse");
         //fdfdf
-        System.out.println("hihihih");
+        System.out.println("hihi");
     }
 }
