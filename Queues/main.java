25a9a686cc77c86a5f7457ed97227d003e89f221
Praveen Pushkar <praveenpushkar140@gmail.com>
2023-01-05 03:03:33 +0530

    second commit

--- Queues/main.java
+++ Queues/main.java
@@ -1,7 +1,7 @@
 package Queues;
-image.png
+
 public class main {
     public static void main(String[] args){
-        System.out.println("hello world");
+        System.out.println("hello worldse");
     }
 }
